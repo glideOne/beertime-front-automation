9a81b575bf20fe779f940432e87e221c31f3276e
Ovidiu Zakarias <ovidiu.zakarias@pitechnologies.ro>
2017-05-29 17:35:39 +0300

    test

--- src/test/java/com/beertime/sixteensixtyfour/base/AbstractTest.java
+++ src/test/java/com/beertime/sixteensixtyfour/base/AbstractTest.java
@@ -56,6 +56,8 @@ public abstract class AbstractTest extends AbstractTestNGSpringContextTests {
         }
     }
 
+    //test
+
     @AfterClass
     public void quitDriver() {
         driver.quit();
